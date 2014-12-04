8472d450e3ea6c87bf68008985aafb2a19a8255b
Damien Regad <dregad@mantisbt.org>
2014-12-04 18:25:50 +0100

    disable CSP

--- core/http_api.php
+++ core/http_api.php
@@ -169,7 +169,7 @@ function http_security_headers() {
 		}
 
 		# Set CSP header
-		header( 'Content-Security-Policy: ' . implode('; ', $t_csp) );
+//		header( 'Content-Security-Policy: ' . implode('; ', $t_csp) );
 
 		if( http_is_protocol_https() ) {
 			header( 'Strict-Transport-Security: max-age=7776000' );

9c57ad319ba5495ea8525ead474ff26f5f651f69
Damien Regad <dregad@mantisbt.org>
2019-08-05 02:11:11 +0200

    TEMP: local bootstrap file as generated by travis config

---
+++ tests/bootstrap_travis.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS['MANTIS_TESTSUITE_SOAP_ENABLED'] = true;
+$GLOBALS['MANTIS_TESTSUITE_SOAP_HOST'] = 'http://localhost/mantis/api/soap/mantisconnect.php?wsdl';
+
+$GLOBALS['MANTIS_TESTSUITE_REST_ENABLED'] = true;
+$GLOBALS['MANTIS_TESTSUITE_REST_HOST'] = 'http://localhost/mantis/api/rest/';
+$GLOBALS['MANTIS_TESTSUITE_API_TOKEN'] = 'WHrfaC6f4w8AcEIbfVm8aJraoECUD6kT';

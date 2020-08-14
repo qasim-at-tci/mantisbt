ee0d79a3ba2c8897a2dcb71cb3b74c5b4fde2531
Damien Regad <dregad@mantisbt.org>
2020-08-14 02:31:43 +0200

    DocBook to AsciiDoc conversion script

---
+++ doc/docbook2asciidoc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+GIT_ROOT=$(git rev-parse --show-toplevel)
+TARGET="$GIT_ROOT/doc"
+
+case ${1:0:1} in
+  "d")
+    GUIDE=Developers_Guide
+    TARGET="$TARGET/developers"
+    mkdir -p $TARGET
+    ;;
+  "a"|*)
+    GUIDE=Admin_Guide
+    TARGET="$TARGET/admin"
+    mkdir -p "$TARGET/config"
+    ;;
+esac
+
+echo "Converting $GUIDE from DocBook to AsciiDoc"
+
+SOURCE="$GIT_ROOT/docbook/$GUIDE/en-US"
+cd $SOURCE || exit 1
+
+echo "Running Pandoc on DocBook files in $SOURCE"
+
+find -name "*.xml" |
+while read filename
+do
+  pandoc "$filename" -f docbook -t asciidoc -s --atx-header -o "$TARGET/${filename%.xml}.adoc"
+done
+echo "Converted AsciiDoc files saved in $TARGET"
